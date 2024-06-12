5af2903f3599245f74c3fd690d2a3a2c3123e7b4
B0neS69 <gavrylyukav479@gmail.com>
2024-06-12 22:07:23 +0300

    myapp

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,46 +8,37 @@ import './Home.css';
 const Home = () => {
   return (
     <Carousel>
-
       <Carousel.Item>
-
         <img 
           className="d-block w-100"
-          src={pcreg}          alt="First slide"
+          src={pcreg}          
+          alt="First slide"
         />
-        
         <Carousel.Caption>
           <h5 >Ремонт та чистка ПК</h5>
           <p>Проводимо професійний ремонт ПК та чистку від пилу</p>
         </Carousel.Caption>
-
       </Carousel.Item>
-
       <Carousel.Item>
-
         <img
           className="d-block w-100"
-          src={pcpg}          alt="Second slide"
+          src={pcpg}          
+          alt="Second slide"
         />
-
         <Carousel.Caption>
           <h5>Встановлення ПЗ</h5>
           <p>Видалимо віруси та встановимо будь-яке ПЗ</p>
         </Carousel.Caption>
       </Carousel.Item>
-
       <Carousel.Item>
-
         <img
           className="d-block w-100"
-          src={pcnew}          alt="Third slide"
+          src={pcnew}          
+          alt="Third slide"
         />
-
         <Carousel.Caption>
           <h5>Збірка ПК</h5>
-          <p>
-            Допоможимо вам підібрати ПК під ваші вимоги
-          </p>
+          <p>Допоможимо вам підібрати ПК під ваші вимоги</p>
         </Carousel.Caption>
       </Carousel.Item>
 
